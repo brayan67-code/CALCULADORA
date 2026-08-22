2e691d259c3e227a57d476af7f0bdacf1a4f4fda
Alexanders030 <167349224+Alexanders030@users.noreply.github.com>
2026-08-22 16:35:56 -0500

    resta version1

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+def restar(numero1, numero2):
+    return numero1 - numero2
+
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+
+resultado = numero1 - numero2
+
+print("El resultado de la resta es:", resultado)
