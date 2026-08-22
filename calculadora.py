ebc1ae72c7a83e31f37b9b1f634f2e52942aef86
brayan67-code <brayanstevenlopezcelis@gmail.com>
2026-08-22 15:55:55 -0600

    suma v2

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
-numero1 = int(input("Ingrese el primer número: "))
-numero2 = int(input("Ingrese el segundo número: "))
+sumando1 = int(input("Ingrese el primer número: "))
+sumando2 = int(input("Ingrese el segundo número: "))
 
-suma = numero1 + numero2
+suma = sumando1 + sumando2
 
 print("La suma es:", suma)
